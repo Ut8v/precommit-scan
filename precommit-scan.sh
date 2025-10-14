efa3247979e61e2201564011c300d4c1e19f7c0e
Utshav Khatiwada <utshavkhatiwada@MacBook-Pro-178.lan>
2025-10-14 16:00:29 -0400

    initial show help function

---
+++ precommit-scan.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+show_help() {
+    echo "usage: $0 [-h] [-v]"
+    echo " "
+    echo "Options: "
+    echo " -h Show this help message"
+    echo " -v Verbose mode" 
+}
+
+show_help
